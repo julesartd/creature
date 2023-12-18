355254930d75930e3b970c994d94c572a4239c82
Jules Artaud <jules.artaud@outlook.com>
2023-12-18 09:25:22 +0100

    add promo

--- src/main/java/creature/reduction/Reduction.java
+++ src/main/java/creature/reduction/Reduction.java
@@ -1,9 +1,20 @@
 package creature.reduction;
 
+import creature.Creature;
+import creature.boost.TypeBoost;
 import creature.skills.Skills;
 
 public abstract class Reduction extends Skills {
+    private double demultiplicateur;
+    private TypeBoost propriete;
+
+    public Reduction(int maxUses, double demultiplicateur, TypeBoost propriete) {
+        super(maxUses);
+        this.demultiplicateur = demultiplicateur;
+        this.propriete = propriete;
+    }
 
 
+    public abstract void use(Creature creature, Creature enemy);
 
 }
--- src/main/java/creature/reduction/items/Promo.java
+++ src/main/java/creature/reduction/items/Promo.java
@@ -1,4 +1,18 @@
 package creature.reduction.items;
 
-public class Promo {
+import creature.Creature;
+import creature.boost.TypeBoost;
+import creature.reduction.Reduction;
+
+public class Promo extends Reduction {
+    private final double demultiplicateur = 0.6;
+
+    public Promo(int maxUses, double demultiplicateur, TypeBoost propriete) {
+        super(maxUses, demultiplicateur, propriete);
+    }
+
+    @Override
+    public void use(Creature creature, Creature enemy) {
+
+    }
 }
