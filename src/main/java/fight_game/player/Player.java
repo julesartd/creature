3b8bd9e8444099aca54be1ecd8f47918ca1e3a02
Jules Artaud <jules.artaud@outlook.com>
2023-12-18 16:38:42 +0100

    add player class

---
+++ src/main/java/fight_game/player/Player.java
@@ -0,0 +1,18 @@
+package fight_game.player;
+
+public class Player {
+    private static Player player;
+    private String name;
+
+    public static Player getInstance() {
+        if (player == null) {
+            player = new Player();
+        }
+        return player;
+    }
+
+    private Player() {
+    }
+
+
+}
